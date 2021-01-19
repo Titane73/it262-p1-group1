755cbc0c3b83a925c52fc55b32bd71dbebd34dd1
Dominick Nelson <60334994+nicknelson97@users.noreply.github.com>
2021-01-18 16:40:24 -0800

    Add files via upload
    added my temp conversion php, as well as error messages and results

--- index.php
+++ index.php
@@ -4,8 +4,53 @@
 // add php here
 // also add php in errors and results divs, 
 //   current error/result text is dummy text for example
+if($_SERVER['REQUEST_METHOD'] == 'POST'){//if satement for error statements
+    if(empty($_POST['temp'])){
+        $errorTemp = "Please give a starting temperature";
+    } else{
+       $temp = $_POST['temp'];
+        
+        $fToC = ($temp - 32) * (5 / 9); //Fahren to Celsius formula
+        $cToK = $temp + 273.15; //Celsius to Kelvin formula
+        $kToF = ($temp - 273.15) * (9 / 5) + 32; //Kelvin to Fahren formula
 
+        $fToK; //Fahren to Kelvin formula
+        $cToF; //Celsius to Fahren formula
+        $ktoC; //Kelvin to Celsius formula
+    }
+    
+    if(empty($_POST['StartScale'])){
+        $errorStart = "Please choose a Starting Scale";
+    }else{
+        $StartScale = $_POST['StartScale'];  
+    }
+    
+    if(empty($_POST['ConvScale'])){
+        $errorConv = "Please choose a Conversion Scale";
+    }else{
+        $ConvScale = $_POST['ConvScale'];
+    }
+}//end error if
 
+if($StartScale == "Fahrenheit"){ //if first scale is Fahren
+    if($ConvScale == "Celsius"){
+        $endTemp = $fToC; //converts to Celsius
+    }else{
+        $endTemp = $fToK;
+    }
+}else if($StartScale == "Celsius"){ //if first scale is Celsius
+    if($ConvScale == "Fahrenheit"){
+        $endTemp = $cToF;
+    }else{
+        $endTemp = $cToK;
+    }
+}else{ //if first scale is Kelvin
+    if($ConvScale == "Fahrenheit"){
+        $endTemp = $kToF;
+    }else{
+        $endTemp = $kToC;
+    }
+}
 
 ?>
 
@@ -41,18 +86,18 @@
                     <fieldset>
                         <legend>Starting Scale</legend>
     
-                        <label><input type="radio" name="StartScale" value="StartF" /> Fahrenheit</label>
-                        <label><input type="radio" name="StartScale" value="StartC" /> Celsius</label>
-                        <label><input type="radio" name="StartScale" value="StartK" /> Kelvin</label>
+                        <label><input type="radio" name="StartScale" value="Fahrenheit" /> Fahrenheit</label>
+                        <label><input type="radio" name="StartScale" value="Celsius" /> Celsius</label>
+                        <label><input type="radio" name="StartScale" value="Kelvin" /> Kelvin</label>
                             
                     </fieldset>
     
                     <fieldset>
                         <legend>Conversion Scale</legend>
     
-                        <label><input type="radio" name="ConvScale" value="ConvF" /> Fahrenheit</label>
-                        <label><input type="radio" name="ConvScale" value="ConvC" /> Celsius</label>
-                        <label><input type="radio" name="ConvScale" value="ConvK" /> Kelvin</label>
+                        <label><input type="radio" name="ConvScale" value="Fahrenheit" /> Fahrenheit</label>
+                        <label><input type="radio" name="ConvScale" value="Celsius" /> Celsius</label>
+                        <label><input type="radio" name="ConvScale" value="Kelvin" /> Kelvin</label>
     
                     </fieldset>
                 </div>
@@ -62,14 +107,16 @@
             </form>
     
             <div class="errors">
-                <p>Error, Will Robinson! Error!</p>
+                <?php echo '<p>'.$errorStart.'</p>' ;?>
+                <?php echo '<p>'.$errorConv.'</p>' ;?>
+                <?php echo '<p>'.$errorTemp.'</p>' ;?>
             </div>
     
             <div class="results">
-                <p>Your starting value was: 32</p>
-                <p>Your starting scale was: Fahrenheit</p>
-                <p>Your conversion scale was: Celsius</p>
-                <p>Your converted value is: 0</p>
+                <?php echo '<p>Your starting value was: '.$temp.'</p>';?>
+                <?php echo '<p>Your starting scale was: '.$StartScale.'</p>';?>
+                <?php echo '<p>Your conversion scale was: '.$ConvScale.'</p>';?>
+                <?php echo '<p>Your converted value is: '.$endTemp.'</p>';?>
             </div>
 
         </div> <!-- end container -->
